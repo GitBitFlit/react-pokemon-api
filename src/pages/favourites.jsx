5c9ced71d82e8946edf4982711356de018c8f5eb
GitBitFlit <58368237+GitBitFlit@users.noreply.github.com>
2022-09-20 22:54:29 +0100

    tweaks + favourite

--- src/pages/favourites.jsx
+++ src/pages/favourites.jsx
@@ -1,5 +1,13 @@
 const Favourites = () => {
-  return <div>favourites</div>;
+  return (
+    <>
+      <div>favourites</div>
+      <div>
+        map favourited pokemon, enable click to view detail, remove button, show
+        date added?
+      </div>
+    </>
+  );
 };
 
 export default Favourites;
