f6ca8f3fdbb559719770c438573179dd4ed30688
GitBitFlit <58368237+GitBitFlit@users.noreply.github.com>
2022-09-22 14:23:28 +0100

    remove unused import statements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "src/styles/index.css";
-import NavBar from "./components/navbar";
-import Pokemon from "./pages/pokemon";
-import Poke from "./pages/poke";
 import App from "./App";
-import Favourites from "./pages/favourites";
 import reportWebVitals from "./reportWebVitals";
 import "font-awesome/css/font-awesome.css";
 
