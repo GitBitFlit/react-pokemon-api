1db7bb8493aafec25f1711217f8292db8e785397
GitBitFlit <58368237+GitBitFlit@users.noreply.github.com>
2022-09-28 10:11:19 +0100

    clear commented code on app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 import "src/styles/index.css";
 import NavBar from "./components/navbar";
 import Pokemon from "./pages/pokemon";
 import Poke from "./pages/poke";
-// import App from "./App";
 import Favourites from "./pages/favourites";
-// import reportWebVitals from "./reportWebVitals";
 import "font-awesome/css/font-awesome.css";
 
 function App() {
@@ -17,14 +14,8 @@ function App() {
 
   useEffect(() => {
     const data = window.localStorage.getItem("favouritePokemon");
-    console.log(data, "get data"); // why is it empty?
-    // setFavouritePokemon(JSON.parse(data));
 
     if (data !== null) {
-      //   localStorage.getItem(
-      //     "favouritePokemon",
-      //     JSON.stringify(favouritePokemon)
-      //   );
       setFavouritePokemon(JSON.parse(data));
       console.log(favouritePokemon, "load persist storage");
     }
@@ -35,50 +26,31 @@ function App() {
       "favouritePokemon",
       JSON.stringify([...favouritePokemon])
     );
-    //   }, [favouritePokemon]);
     console.log(favouritePokemon, "useEffect set Fave Pokemon");
   }, [favouritePokemon]);
 
-  //   var retrievedObject = localStorage.getItem("favouritePokemon");
-  //   console.log("retrievedObject: ", JSON.parse(retrievedObject));
   let handleFavouriteClass = "clickable fa fa-heart";
 
   const handleAddToFavourites = (poke) => {
-    // console.log(favouritePokemon, "fave poke");
     const findPoke = favouritePokemon.find((p) => p.name === poke.name);
     if (!findPoke) {
-      // const favePokemon = { ...favouritePokemon };
-      // console.log(favePokemon);
-      // favePokemon.push(poke);
       console.log("fave icon");
       setFavouritePokemon((favouritePokemon) => [poke, ...favouritePokemon]);
       handleFavouriteClass += "-o";
       console.log(handleFavouriteClass, "heart icon");
-      // setFavouritePokemon(...favouritePokemon, poke);
     }
 
     if (findPoke) {
-      // remove from list
       const favouritedPokemon = [...favouritePokemon];
       const index = favouritedPokemon.findIndex((p) => p.url === poke.url);
-      // splice
       const removedPokemon = favouritedPokemon.splice(index, 1);
       console.log(removedPokemon, "removed pokemon");
-      // update state
       setFavouritePokemon(favouritedPokemon);
     }
-
-    // console.log(findPoke, "poke found?");
-    // console.log(poke, "poke to add to favourites");
   };
 
   const handleRemoveFromFavourites = (poke) => {
     console.log(poke, "poke to remove from favourites");
-    // const index = favouritePokemon.findIndex((p) => p.url === poke.url);
-    // console.log(index, "index to remove");
-    // favouritePokemon.splice(index, 1);
-    // setFavouritePokemon(favouritePokemon);
-
     let filteredFavouriteList = favouritePokemon.filter(
       (p) => p.url !== poke.url
     );
@@ -87,9 +59,7 @@ function App() {
 
   const onFavouriteClass = (name) => {
     const findPoke = favouritePokemon.find((p) => p.name === name);
-    // if (!findPoke) {
     return findPoke ? "clickable fa fa-heart" : "clickable fa fa-heart-o";
-    // return "fa fa-heart";
   };
 
   const formatName = (name) => {
@@ -113,17 +83,6 @@ function App() {
   return (
     <>
       <NavBar />
-
-      {/* <div>
-        {favouritePokemon.map((poke) => {
-          return (
-            <div>
-              <p key={poke.url}>{poke.name}</p>
-            </div>
-          );
-        })}
-      </div> */}
-
       <Routes>
         <Route
           path="/pokemon/:pokeId"
